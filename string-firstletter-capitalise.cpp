c323396135798265c7ac5c227c2dce202ff6552a
Jaiveer Trivedi <132746092+MASJV@users.noreply.github.com>
2024-08-01 08:04:55 +0530

    String's first Letter capitalise

--- string-firstletter-capitalise.cpp
+++ string-firstletter-capitalise.cpp
@@ -6,7 +6,7 @@ int main(){
     cin>>input;
 
     int a = 0;
-    for(int i=0;i<input.length();i++){
+    for(int i=0;i<=input.length();i++){
         while(input[i] != '\0'){
             input[a]= toupper(input[a]);
         }
